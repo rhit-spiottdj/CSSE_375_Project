bd89ec8ffb4b5cab0eac10861f6b5fde06a4a86a
ctf05 <christianbeadling@protonmail.com>
2025-04-03 15:49:05 -0400

    split up methods for smaller methods

--- src/main/java/domain/ProgressCardManager.java
+++ src/main/java/domain/ProgressCardManager.java
@@ -1,47 +1,53 @@
 package domain;
-
-
 import java.util.Collection;
-
 public class ProgressCardManager {
-
     Bank bank;
 
     ProgressCardManager(Bank bank){
         this.bank = bank;
     }
 
-
     public Collection<ResourceType> playYearOfPlenty(
-        Player player, ResourceType resourceOne, ResourceType resourceTwo) {
-
-        boolean insufficientResources = false;
-        if(resourceOne.equals(resourceTwo)){
-            insufficientResources = bank.noMoreResource(resourceOne,2);
-        }else{
-            insufficientResources = bank.noMoreResource(resourceOne, 1) ||
-                bank.noMoreResource(resourceTwo, 1);
+            Player player, ResourceType resourceOne, ResourceType resourceTwo) {
+        if (!checkSufficientResources(resourceOne, resourceTwo)) {
+            return player.getResources();
         }
-        if(!insufficientResources){
-            try {
-                player.addResource(resourceOne);
-                player.addResource(resourceTwo);
-            }catch(IllegalStateException e){
+        addResourcesToPlayer(player, resourceOne, resourceTwo);
+        return player.getResources();
+    }
+
+    private boolean checkSufficientResources(ResourceType resourceOne, ResourceType resourceTwo) {
+        if (resourceOne.equals(resourceTwo)) {
+            return !bank.noMoreResource(resourceOne, 2);
+        } else {
+            return !(bank.noMoreResource(resourceOne, 1) ||
+                    bank.noMoreResource(resourceTwo, 1));
+        }
+    }
+
+    private void addResourcesToPlayer(Player player, ResourceType resourceOne, ResourceType resourceTwo) {
+        try {
+            player.addResource(resourceOne);
+            player.addResource(resourceTwo);
+        } catch(IllegalStateException e) {
+            // Original code swallows the exception
+        }
+    }
+
+    public Collection<ResourceType> playMonopoly(
+            Collection<Player> players, Player player, ResourceType resource) {
+        for (Player checkPlayer : players) {
+            if (checkPlayer.equals(player)) {
+                continue;
             }
+            transferResources(checkPlayer, player, resource);
         }
         return player.getResources();
     }
 
-    public Collection<ResourceType> playMonopoly(
-        Collection<Player> players, Player player, ResourceType resource) {
-        for(Player checkPlayer: players){
-            if(checkPlayer.equals(player)){
-                continue;
-            }
-            while (checkPlayer.removeResource(resource)){
-                player.addResource(resource);
-            }
+    private void transferResources(Player fromPlayer, Player toPlayer, ResourceType resource) {
+        while (fromPlayer.removeResource(resource)) {
+            toPlayer.addResource(resource);
         }
-        return player.getResources();
     }
 }
